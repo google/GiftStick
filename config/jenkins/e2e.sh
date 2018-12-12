6fa54adc58724ed6012d8d1e965cb67cf456efa4
Romain Gayon <romaing@google.com>
2018-12-12 18:50:02 +0100

    + deps

--- config/jenkins/e2e.sh
+++ config/jenkins/e2e.sh
@@ -63,7 +63,8 @@ EOAPT
 
   apt update -y
   apt install -y \
-    grub-efi-amd64-bin=2.02-2ubuntu8
+    grub-efi-amd64-bin=2.02-2ubuntu8 \
+    grub-common=2.02-2ubuntu8
 
   if [ ! -f "${ISO_FILENAME}" ]; then
     wget -q -nc -O "${ISO_FILENAME}" "${ISO_TO_REMASTER_URL}"
