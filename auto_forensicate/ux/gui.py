0a2b5f8074470219cc44691578dd623c0924aa9d
Romain Gayon <romaing@google.com>
2019-08-19 18:11:38 +0200

    Add yes/no question GUI & some cleanup (#55)
    * Add yes/no question GUI & some cleanup
    
    * Update auto_forensicate/ux/zenity.py

--- auto_forensicate/ux/gui.py
+++ auto_forensicate/ux/gui.py
@@ -68,3 +68,14 @@ def AskDiskList(disk_list):
   if choices == ['']:
     return []
   return [disk_description_map[choice] for choice in choices]
+
+
+def Confirm(text):
+  """Asks the user to confirm something.
+
+  Args:
+    text(str): the text of the question.
+  Returns:
+    bool: True if the user confirms, False otherwise.
+  """
+  return zenity.GetYesNo(text)
